b9623d311cdddc5af505a7ef72153e9f9db13e55
Dileep.B <dileep.b@fsoft.com.vn>
2023-11-29 15:40:32 +0530

    Updated url

--- src/app/pages/landing-page.js
+++ src/app/pages/landing-page.js
@@ -10,7 +10,7 @@ const LandingPage = () => {
     //const templatePath = window.location.hostname + window.location.pathname
     //const templateURL = `http://${window.location.hostname}${templatePath}`;
     // const templateURL = encodeURIComponent(window.location.pathname.substring(1))
-    const templateURL = encodeURIComponent(window.location.ancestorOrigins[0] + window.location.ancestorOrigins);
+    const templateURL = encodeURIComponent(window.location.ancestorOrigins[0]);
     
     console.log('templateURL',templateURL);
     const templateDetailsData = useSelector(state => state.getTemplateDetailsReducer?.data)
