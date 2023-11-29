a139e64797c3a35e8406a934dd5a9949c83d8aa5
saibhargav.g <saibhargav.g@fsoft.com.vn>
2023-11-29 16:47:18 +0530

    description alignment

--- src/app/pages/landing-page.js
+++ src/app/pages/landing-page.js
@@ -7,9 +7,6 @@ import UrlTemplate from './url-template';
 const LandingPage = () => {
 
     const dispatch = useDispatch()
-    //const templatePath = window.location.hostname + window.location.pathname
-    //const templateURL = `http://${window.location.hostname}${templatePath}`;
-    // const templateURL = encodeURIComponent(window.location.pathname.substring(1))
     let templateURL = window.location.ancestorOrigins[0];
     templateURL = templateURL.replace(/^https?:\/\//, '');
     const templateDetailsData = useSelector(state => state.getTemplateDetailsReducer?.data)
