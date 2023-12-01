2325ae6f9c136443e3b756628e057b72e9244547
saibhargav.g <saibhargav.g@fsoft.com.vn>
2023-12-01 18:39:41 +0530

    removed comments

--- src/services/api/axiosClient.js
+++ src/services/api/axiosClient.js
@@ -4,7 +4,7 @@ export const axiosClient = axios.create({
   baseURL: "http://swish-be-dev-lb-1683661170.us-east-2.elb.amazonaws.com",
   timeout: 30 * 60 * 1000,
   headers: {
-    'Content-Type': 'application/json', // Corrected content type for JSON
+    'Content-Type': 'application/json',
   },
 });
 
