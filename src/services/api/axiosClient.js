d36d70ee323165a29510d97c954c806ca00c09b9
saibhargav.g <saibhargav.g@fsoft.com.vn>
2023-12-05 15:06:00 +0530

    axios revert

--- src/services/api/axiosClient.js
+++ src/services/api/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-  baseURL: "http://swish-landing-page-dev-2035960663.us-east-2.elb.amazonaws.com",
+  baseURL: "http://swish-be-dev-lb-1683661170.us-east-2.elb.amazonaws.com",
   timeout: 30 * 60 * 1000,
   headers: {
     'Content-Type': 'application/json',
